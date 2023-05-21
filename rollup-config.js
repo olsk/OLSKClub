53b9016a81da4c2467eb66d4edf2e96608c6b24e
Rosano Coutinho <pub@xeari.com>
2023-05-21 14:44:27 +0700

    Purge module

--- rollup-config.js
+++
@@ -1,1 +0,0 @@
-export default require('OLSKRollupScaffold').OLSKRollupScaffoldScanStart(__dirname);
--- rollup-start.js
+++
@@ -1,9 +0,0 @@
-import RollupStart from './main.svelte';
-
-const OLSKClub = new RollupStart({
-	target: document.body,
-	props: Object.assign({
-	}, Object.fromEntries(Array.from((new window.URLSearchParams(window.location.search)).entries()))),
-});
-
-export default OLSKClub;
